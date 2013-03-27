a37eb9c50d43c8757cc5e2c820e3f78fcdb184d3
Olav Vahtras <vahtras@kth.se>
2013-03-27 07:11:50 +0100

    rspvec test case

---
+++ test/test_rspvec.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+from daltools import rspvec, sirifc
+
+def setup():
+    global tmpdir, RSPVEC, ifc
+    n, e = os.path.splitext(__file__)
+    tmpdir = n + ".d"
+    RSPVEC = os.path.join(tmpdir, 'RSPVEC')
+    SIRIFC = os.path.join(tmpdir, 'SIRIFC')
+    ifc = sirifc.sirifc(name=SIRIFC)
+
+def assert_(this, ref):
+    print this, ref
+    assert np.allclose(this, ref)
+
+def test_read():
+    Nx = rspvec.read("XDIPLEN", RSPVEC)
+    this = Nx[12]
+    ref = -0.75732690
+    assert_(this, ref)
+
+def test_tomat():
+    Nx = rspvec.read("XDIPLEN", RSPVEC)
+    kx = rspvec.tomat(Nx, ifc, tmpdir=tmpdir)
+    this = kx[8,3]
+    ref = 0.75732690
+    assert_(this, ref)
+
+if __name__ == "__main__":
+    setup()
