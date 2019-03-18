a1242fb8fd3a5a53b22fa693405108b0a7f6a2cd
Olav Vahtras <vahtras@kth.se>
2019-03-18 06:54:50 +0100

    drop Python2 support

--- daltools/__init__.py
+++ daltools/__init__.py
@@ -1,2 +1,10 @@
+import sys
+
 __version__ = "1.0.1"
-#from . import one, sirrst, prop
+
+
+def verify_version():
+    assert sys.version_info >= (3, 6), 'Python version < 3.6 not supported'
+
+
+verify_version()
